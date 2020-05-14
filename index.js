3bbcda22e7c54992db1352826dfe91d4807ad833
Sharlene May <sharleneminosa@gmail.com>
2020-05-14 14:25:50 -0700

    generating readme

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-var inquirer = require("inquirer");
-var fs = require ("fs");
+const fs = require ("fs");
+// const axios = require("axios");
+const inquirer = require("inquirer");
 
 const questions = [
     {
         type: "input",
+        
         message: "What is your project titled?",
         name: "title"
     },
@@ -18,36 +20,116 @@ const questions = [
     {
         type: "input",
         message: "Please provide a description of your project.",
-        name: "description",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "What is the method of installation?",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Usage:",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "What are the languages being used?",
+        name: "contributing"
+    },
+    {
+        type: "input",
+        message: "Tests:",
+        name: "tests"
+    },
+    {
+        type:"input",
+        message: "Enter your GitHub username:",
+        name: "username"
+    },
+    {
+        type:"input",
+        message: "What is your email?",
+        name: "email"
     }
 ];
 
-inquirer
-  .prompt(
-    questions
-)
-.then (function(data) {
-
-    var filename = "README.md";
-
-    fs.writeFile(filename,JSON.stringify(data, null, '\t'),function(err){
-        if(err){
-            return console.log(err);
-        }
-
-        console.log("Success!");
+function init() {
+    inquirer.prompt(questions)
+    .then( response => {
+        const info = generateReadMe(response)
+            fs.writeFileSync("README.md",info);
     });
+}
 
-});
+function generateReadMe(data) {
+    return `
+  
+  [![License: ${data.license}](https://img.shields.io/badge/License-${data.license}-blue.svg)](https://opensource.org/licenses/${data.license})
+  # ${data.title}
+  
+  
+  ## Description
+  
+  ${data.description}
+  
+  ## Table of Contents
+  *[Installation](#installation)
+  *[Usage](#usage)
+  *[License](#license)
+  *[Contributing](#contributing)
+  *[Tests](#tests)
+  *[Questions](#questions)
+  
+  ## Installation
+  
+  How to install: ${data.installation}
+  
+  ## Usage
+  
+  ${data.usage}
+  
+  ## License
+  
+  A ${data.license} license is used.
+  
+  ## Contributing
+  
+  Languages Used: ${data.contributing}
+  
+  ## Tests
+  
+  ${data.tests}
+  
+  ## Questions
+  
+  Please contact ${data.username} at ${data.email} for questions. 
+  
+  `
+  }
 
-// function writeToFile(fileName, data) {
-// }
-
-// function init() {
-
-// }
-
-// init();
+init();
 
     // contributing: languages used, frameworks used, any knowledge necessary for contribution by another user
     // tests: npm test
+
+    // (function({ username }) {
+    //     const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+    
+    //     axios.get(queryUrl)
+    //     .then(function(res){
+    //         const repoNames = res.data.map(function(repo){
+    //             return repo.name;
+    //         });
+    
+    //         const repoNamesStr = repoNames.join("\n");
+    
+    //         fs.writeFile("repos.text", repoNamesStr, function(err){
+    //             if (err) {
+    //                 throw err;
+    //             }
+    
+    //             console.log(`Saved ${repoNames.length} repos.`);
+    //         });
+    //     });
+    // });
